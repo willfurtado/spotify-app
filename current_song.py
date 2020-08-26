fd7c8f7209be81713eccb01e9f4b0edbdfbff901
Will Furtado <willfurtado@berkeley.edu>
2020-08-26 10:02:53 -0700

    reverted to secrets

--- current_song.py
+++ current_song.py
@@ -1,5 +1,6 @@
 import spotipy
 import json
+from secrets import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-read-currently-playing"
--- top_songs.py
+++ top_songs.py
@@ -1,5 +1,6 @@
 import spotipy
 import json
+from secrets import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-top-read"
